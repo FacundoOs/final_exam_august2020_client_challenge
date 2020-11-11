aaec85a4aba2b68a24aee71c1b4e97acb6baf6c1
FacundoOs <osores.facundo@gmail.com>
2020-11-11 16:02:43 +0100

    Add get request to json file and display the data

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <> 
-    </>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default App
+  useEffect(() => {
+    const fetchData = async () => {
+      let courses = await axios.get("../src/data/courses.js");
+      setData(courses);
+    };
+    fetchData();
+  }, []);
+
+  let dataShow = data.map((show) => {
+    return (
+      <div>
+        <div>{show.title} </div>
+        <div>{show.category} </div>
+        <div>{show.instructors} </div>
+        <div>{show.price} </div>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div>{dataShow}</div>
+      <ShowCourse />
+    </>
+  );
+};
+
+export default App;
